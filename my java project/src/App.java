97566dede36e5375787bd6e161642b17fbc7d001
Elsam Atchole <elsamatchole@gmail.com>
2022-11-07 12:51:29 +0000

    my java stuff

---
+++ my java project/src/App.java
@@ -0,0 +1,11 @@
+import java.lang.Integer;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+   Scanner scanner = new Scanner(System.in) ;
+   byte age = scanner.nextByte();
+   System.out.println("You are " + age);
+}
+
+}
